b33782219e523d9d4995e3ccda5bd4c31660d40f
Puentebravo <puentematos@gmail.com>
2021-04-13 17:06:49 -0400

    Fixed routing, added imports

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Search from "./pages/search/search";
+import Saved from "./pages/saved/saved";
 import "./App.css";
 
 function App() {
   return (
     <Router basename={`${process.env.PUBLIC_URL}/`}>
-        <Switch>
-          <Route exact path="/" component={Index} />
-          <Route exact path="/home" component={Index} />
-        </Switch>
+      <Switch>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/home" component={Search} />
+        <Route exact path="/search" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Switch>
     </Router>
   );
 }
