760a9d12eee692b6e134353249113f42415c3ac3
Puentebravo <puentematos@gmail.com>
2021-04-12 21:26:13 -0400

    Added header boilerplate

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,1 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div className="container.fluid">
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <Link className="navbar-brand" to="/">
+          Charlie Puente
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li
+              className={
+                window.location.pathname === "/" ||
+                window.location.pathname === "/home"
+                  ? "nav-item active"
+                  : "nav-item"
+              }
+            >
+              <Link className="nav-link" to="/">
+                <i className="fas fa-id-card"></i> Home
+              </Link>
+            </li>
+            <li
+              className={
+                window.location.pathname === "/portfolio"
+                  ? "nav-item active"
+                  : "nav-item"
+              }
+            >
+              <Link className="nav-link" to="/portfolio">
+                <i className="fas fa-atom"></i> Portfolio
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
