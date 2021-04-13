a4b1a051e72931c7bf652af8a8655a589d0dffcd
Puentebravo <puentematos@gmail.com>
2021-04-12 22:00:19 -0400

    Added static title element

---
+++ client/src/components/lead.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Lead() {
+    return (
+        <div className="row justify-content-center">
+            <div className="card">
+                <h1 className="display-2">Google Books Search</h1>
+                <p className="lead">Find your next favorite read!</p>
+            </div>
+        </div>
+    )
+}
+
+export default Lead;
