07ee616bd87f079536168e623b416af28cc64467
Puentebravo <puentematos@gmail.com>
2021-04-14 00:59:39 -0400

    Added mongoDB connection code, routes import

--- server.js
+++ server.js
@@ -2,13 +2,23 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
 
 require("dotenv").config();
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/savedbooks", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+
+app.use(require("./db/routes.js"))
 
 // Send every request to the React app
 // Define any API routes before this runs
