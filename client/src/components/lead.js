d16d59e52f27c7ae11bfaa28528e072d275b2a4d
Puentebravo <puentematos@gmail.com>
2021-04-19 14:23:15 -0400

    Added unique ID to lead element for styling

--- client/src/components/lead.js
+++ client/src/components/lead.js
@@ -3,7 +3,7 @@ import React from "react";
 function Lead() {
     return (
         <div className="row justify-content-center">
-            <div className="card">
+            <div className="card" id="leadEl">
                 <h1 className="display-2">Google Books Search</h1>
                 <p className="lead">Find your next favorite read!</p>
             </div>
