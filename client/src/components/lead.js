add0ff665eedf20e44ab7b27b87f3e5d959bda31
Puentebravo <puentematos@gmail.com>
2021-04-19 18:25:01 -0400

    Added unique ID to lead element for custom styling

--- client/src/components/lead.js
+++ client/src/components/lead.js
@@ -5,7 +5,7 @@ function Lead() {
         <div className="row justify-content-center">
             <div className="card" id="leadEl">
                 <h1 className="display-2">Google Books Search</h1>
-                <p className="lead">Find your next favorite read!</p>
+                <p className="lead text-center">Find your next favorite read!</p>
             </div>
         </div>
     )
