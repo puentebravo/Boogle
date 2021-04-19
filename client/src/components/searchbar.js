1cd28abe681e497cf78c12e986f44f1c4c4b7a74
Puentebravo <puentematos@gmail.com>
2021-04-19 14:24:49 -0400

    Added unique id to searchbar for styling

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -2,11 +2,11 @@ import React from "react";
 
 function SearchBar(props) {
   return (
-    <div className="row justify-content-center">
+    <div className="row justify-content-center" id="searchB">
       <form className="d-flex">
         <input
-          value={props.filter}
-          onChange={props.handleInputChange}
+          query={props.search}
+          onSubmit={props.handleInputChange}
           className="form-control me-2"
           type="search"
           placeholder="Search Directory"
