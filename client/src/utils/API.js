4a6255f9467e61c77b5e459c99dd46daaacfe440
Puentebravo <puentematos@gmail.com>
2021-04-19 18:23:55 -0400

    cleaned up API call, removed key requirement

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,10 +1,8 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
-const ENDPOINT = "https://www.googleapis.com/books/v1/volumes?q=";
-const KEY = `&key=${process.env.API_KEY}`;
 
 export default {
   ping: function (query) {
-    return axios.get(ENDPOINT + query + KEY);
+    return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}`);
   },
 };
