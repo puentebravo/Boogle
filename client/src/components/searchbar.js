84c500a66ff7d5d6035e9cf8fad992664ed0b5a2
Puentebravo <puentematos@gmail.com>
2021-04-22 12:50:47 -0400

    Fixed onChange method

--- client/src/components/searchbar.js
+++ client/src/components/searchbar.js
@@ -6,16 +6,13 @@ function SearchBar(props) {
       <form className="d-flex">
         <input
           value={props.query}
-          onSubmit={props.handleInputChange}
+          onChange={props.handleInputChange}
           className="form-control me-2"
           type="search"
           placeholder="Search Books"
           aria-label="Search"
         />
       </form>
-      <button type="submit" className="btn btn-danger">
-        Search
-      </button>
     </div>
   );
 }
