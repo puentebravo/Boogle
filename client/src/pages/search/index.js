9c7315f0b335d4a8f1b05c8d12da1ca61e2d4672
Puentebravo <puentematos@gmail.com>
2021-04-22 14:24:33 -0400

    added functionality to searchbar button

--- client/src/pages/search/index.js
+++ client/src/pages/search/index.js
@@ -7,7 +7,7 @@ import Footer from "../../components/footer";
 import "./style.css";
 
 function Search() {
-  const [search, setSearch] = useState("The Matrix");
+  const [search, setSearch] = useState("The Hunt for Red October");
 
   const handleInputChange = (event) => {
     setSearch(event.target.value);
@@ -15,6 +15,9 @@ function Search() {
 
   const handleFormSubmit = (event) => {
     event.preventDefault();
+    console.log(event.target.value);
+    console.log("Form submitted");
+    setSearch(event.target.value);
   };
 
   return (
@@ -26,7 +29,7 @@ function Search() {
         handleInputChange={handleInputChange}
         query={search}
       />
-      <SearchResults search={search}/>
+      <SearchResults search={search} />
       <Footer />
     </div>
   );
